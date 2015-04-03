138c1db46ccfb7d95b4c1cbc8a7119aee4f33775
WinnerJiang <winner_118@sohu.com>
2015-04-02 20:28:29 -0700

    first project frame

---
+++ main.js
@@ -0,0 +1,15 @@
+/*	
+	send parameter from html to php through Ajax
+*/
+
+
+$(document).ready(function(){
+
+	alert("hello jQuery!");
+	$("#addButton").click(function(){
+		alert("hello addButton!");
+
+	});
+
+});
+
