b2009be0d4d9ae60d9a8d5619a46a62699fee606
WinnerJiang <winner_118@sohu.com>
2015-04-03 01:39:28 -0700

    add function test for running ajax

--- calculate.php
+++ calculate.php
@@ -1,3 +1,22 @@
 <?php
-	
+
+$operation=$_POST['operation'];
+$FirstNum = $_POST['firstNum'];
+$SecondNum = $_POST['secondNum'];
+$Result = 0;
+
+
+if($operation=="add"){
+	$Result = addFunction($FirstNum,$SecondNum);
+	echo json_encode($Result);	
+}
+
+
+function addFunction($a,$b){
+	echo "php 16";
+	$result =0;
+	$result = $a + $b;
+	return $result;
+};
+
 ?>
--- main.js
+++ main.js
@@ -6,10 +6,34 @@
 $(document).ready(function(){
 
 	alert("hello jQuery!");
+	var operation ="";
+	var response="";
+
+	//send add function para
 	$("#addButton").click(function(){
 		alert("hello addButton!");
 
+		//Ajax request to calculate.php
+		operation = "add";
+
+		$.ajax({
+			type:"POST",
+			url:"calculate.php",
+			data:{operation:operation,firstNum:"222",secondNum:"33"},
+			dataType:"json",
+			success:function(data){
+				$.addFunctionCallback(data);
+			}  //notice! cannot directly use $.addFunctionCallback(data);
+		});
+
+		$.addFunctionCallback = function(response){
+			alert("hello addFunctionCallback!");
+			alert(response);
+
+		};
 	});
+
+
 
 });
 
